c460f9130e659f0a0ca75ff772f1860000f2623b
mildred morales <mildredmoralesalmeida1503@gmail.com>
2022-06-03 10:48:34 -0300

    validacao do formulario de contato

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ var navigationHeader = document.getElementById('navigation_header')
 var content = document.getElementById('content');
 var showSidebar = false;
 
-function toggleSidebar() 
-{
+function toggleSidebar() {
     showSidebar = !showSidebar;
 
     if (showSidebar) {
@@ -18,32 +17,103 @@ function toggleSidebar()
         content.style.filter = '';
     }
 }
-    function closeSidebar() {
-        if (showSidebar) {
-            toggleSidebar();
-        }
+function closeSidebar() {
+    if (showSidebar) {
+        toggleSidebar();
     }
+}
+
+window.addEventListener('resize', function (event) {
+    if (window.innerWidth > 790 && showSidebar) {
+
+        toggleSidebar();
+    }
+});
+
+const repoProjects = () => {
+    const url = "https://api.github.com/users/m3psud/repos"
+    fetch(url)
+        .then(resp => resp.json())
+        .then(projects => {
+            console.log(projects)
+        })
+}
+repoProjects()
+document.addEventListener("DOMContentloaded", function startApp() {
+
+})
+
+
+ const form = document.getElementById('form')
+ const nome = document.getElementById('username')
+ const email = document.getElementById('email')
+ const assunto = document.getElementById('assunto')
+ const mensagem = document.getElementById('mensagem')
+
+ form.addEventListener('submit', (e) => {
+   e.preventDefault()
+
+    checkInputs()
+})
+
+ function checkInputs(){
+   
+   
+    const nomeValue = username.value.trim()
+    const emailValue = email.value.trim()
+    const assuntoValue = assunto.value.trim()
+    const mensagemValue = mensagem.value.trim()
+     
+    if (nomeValue === '' ) 
+      {
+        errorValidation(username, 'Preencha esse campo')
+      }
+      else if(nomeValue.length < 3){
+        errorValidation(username, 'O nome deve conter pelo menos 3 caracteres')
+
+      }
+      else 
+      {
+       successValidation(username)
+      }
+
+      if (emailValue === '' ) 
+      {
+        errorValidation(email, 'Preencha esse campo')
+      }
+     
+      else 
+      {
+       successValidation(email)
+      }
+
+      if (assuntoValue === '' ) 
+      {
+        errorValidation(assunto, 'Preencha esse campo')
+      }
+      else if(assuntoValue.length < 3){
+        errorValidation(assunto, 'O assunto deve conter pelo menos 3 caracteres')
+
+      }
+      else 
+      {
+       successValidation(assunto)
+      }
+
+
     
-    window.addEventListener('resize', function (event) {
-        if (window.innerWidth > 790 && showSidebar) {
+}
 
-            toggleSidebar();
-        }
-    });
+function errorValidation(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small')
 
-    const repoProjects = () =>{
-       const url = "https://api.github.com/users/m3psud/repos"
-       fetch(url)
-       .then(resp => resp.json())
-       .then(projects => {
-           console.log(projects)
-       })
-    }
-    repoProjects()
-    document.addEventListener("DOMContentloaded", function startApp (){
-        
-    })
+    small.innerText = message;
+    formControl.className = 'form-control error'
 
-
-       
-      
+}
+ function successValidation(input){
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success'
+    
+ }
